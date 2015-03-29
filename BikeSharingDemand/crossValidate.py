88480aea3171511356ebfdb1d0e7fed3b78c1cb3
adutta <arnab.dutta@gmail.com>
2015-03-30 01:17:02 +0200

    stable submission

--- BikeSharingDemand/crossValidate.py
+++ BikeSharingDemand/crossValidate.py
@@ -1,26 +1,49 @@
 from sklearn import cross_validation
 from sklearn import linear_model
+from sklearn.tree import DecisionTreeRegressor
 from numpy import genfromtxt
+from sklearn.ensemble import GradientBoostingRegressor
+import pandas as pd
+import datetime
 
 
 def main():
     # read in  data, parse into training and target sets
-    dataset = genfromtxt(open('Data/train.csv', 'r'), delimiter=',', dtype='f8')[1:]
-    target = [x[11] for x in dataset]
-    train = [x[1:9] for x in dataset]
-    test = genfromtxt(open('Data/test.csv', 'r'), delimiter=',', dtype='f8')[1:, 1:]
-    print(test.shape)
+
+    # TESTING ##############
+    df_test_orig = pd.read_csv('Data/test.csv', header=0)
+    df_test_temp = pd.read_csv('Data/test.csv', header=0)
+
+    df_test_temp['datetime'] = df_test_temp.apply(lambda row: 12-datetime.datetime.strptime(row['datetime'],
+                                                                                            '%Y-%m-%d %H:%M:%S').hour, axis=1)
+
+    # TRAINING ##############
+    df_train = pd.read_csv('Data/train.csv',  header=0)
+
+    df_train['datetime'] = df_train.apply(lambda row: 12-datetime.datetime.strptime(row['datetime'],
+                                                                                    '%Y-%m-%d %H:%M:%S').hour, axis=1)
+
+    # print(df_train.loc[0:30, 'datetime'])
+    print("DF Dim = " + str(df_train.shape))
+
+    training = df_train.loc[0:, ("atemp", "humidity", "windspeed")]
+    target = df_train.loc[0:, 'count']
+    print("training Dim = " + str(training.shape))
+    print("target Dim = " + str(target.shape))
+
+    test = df_test_temp.loc[0:, ("atemp", "humidity", "windspeed")]
+    print("Test Dim = " + str(test.shape))
 
     # multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
-    regress = linear_model.LinearRegression(fit_intercept=False)
+    regress = DecisionTreeRegressor(max_depth=9)
+    # regress = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=7, random_state=0, loss='ls')
 
     # Simple K-Fold cross validation. 5 folds.
-    cv = cross_validation.KFold(len(train), 5)
+    cv = cross_validation.KFold(len(training), 5)
 
-    scores = cross_validation.cross_val_score(regress, train, target, scoring='r2', cv=cv)
+    scores = cross_validation.cross_val_score(regress, training, target, scoring='mean_absolute_error', cv=cv)
 
-    print("Results: " + scores)
-
+    print(scores.mean())
 
 if __name__ == "__main__":
     main()
--- BikeSharingDemand/makeSubmission.py
+++ BikeSharingDemand/makeSubmission.py
@@ -1,28 +1,49 @@
-from numpy import genfromtxt, savetxt
 from sklearn import linear_model
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+import pandas as pd
+import datetime
+import numpy as np
 
-# https://www.kaggle.com/c/bioresponse
+# https://www.kaggle.com/c/bike-sharing-demand
 
 
 def main():
-    # create the training & test sets, skipping the header row with [1:]
-    dataset = genfromtxt(open('Data/train.csv', 'r'), delimiter=',', dtype='f8')[1:]
-    target = [x[11] for x in dataset]
-    train = [x[1:9] for x in dataset]
-    test = genfromtxt(open('Data/test.csv', 'r'), delimiter=',', dtype='f8')[1:, 1:]
-    print(test.shape)
+    # read in  data, parse into training and target sets
+
+    # TESTING ##############
+    df_test_orig = pd.read_csv('Data/test.csv', header=0)
+    df_test_temp = pd.read_csv('Data/test.csv', header=0)
+
+    df_test_temp['datetime'] = df_test_temp.apply(lambda row: 12-datetime.datetime.strptime(row['datetime'],
+                                                                                            '%Y-%m-%d %H:%M:%S').hour, axis=1)
+
+    # TRAINING ##############
+    df_train = pd.read_csv('Data/train.csv',  header=0)
+
+    df_train['datetime'] = df_train.apply(lambda row: 12-datetime.datetime.strptime(row['datetime'],
+                                                                                    '%Y-%m-%d %H:%M:%S').hour, axis=1)
+
+    print("DF Dim = " + str(df_train.shape))
+
+    training = df_train.loc[0:, 'datetime':'windspeed']
+    target = df_train.loc[0:, 'count']
+    print("training Dim = " + str(training.shape))
+    print("target Dim = " + str(target.shape))
+
+    test = df_test_temp.loc[0:, 'datetime':'windspeed']
+    print("Test Dim = " + str(test.shape))
 
     # create and train the random forest
-    # multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
-    regress = linear_model.LinearRegression(fit_intercept=False)
-    regress.fit(train, target)
-    print(regress.coef_)
+    # multi-core CPUs can use:
+    regress = DecisionTreeRegressor(max_depth=9)
+    # regress = GradientBoostingRegressor(n_estimators=50, learning_rate=0.1, max_depth=2, random_state=0, loss='ls')
 
-    Y_test = regress.predict(test)
-    predicted_counts = [x[1] for x in enumerate(regress.predict(test))]
+    regress.fit(training, target)
 
-    savetxt('Data/submission.csv', predicted_counts, delimiter=',', fmt='%f',
-            header='count', comments='')
+    # output for submission
+    np.savetxt("Data/submission.csv", np.column_stack((df_test_orig['datetime'], regress.predict(test))),
+               delimiter="\t", fmt='%s,%f', header='datetime,count')
 
 
 if __name__ == "__main__":
